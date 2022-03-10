f5bd78fc1f2365ae4e5e4ab765b078b9a47b4ee2
Felipe Cava <felipecavs@gmail.com>
2022-03-10 16:34:45 -0300

    teste

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,16 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
+    const subscriberResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscriberResponseJson = await subscriberResponse.json();
+    const inscritos = subscriberResponseJson.total_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
-        date: dynamicDate.toUTCString()
+        date: dynamicDate.toUTCString(),
+        inscritos: inscritos,
     })
 }
 
